8c674023c780b026717d6ab27bcd60b59fd9ab6c
Oliver Schneider <git-spam-no-reply9815368754983@oli-obk.de>
2017-10-13 11:17:42 +0200

    Adding multiple constants in a single statement can lead to various ordering issues

--- src/librustc/mir/interpret/step.rs
+++ src/librustc/mir/interpret/step.rs
@@ -39,13 +39,13 @@ impl<'a, 'tcx, M: Machine<'tcx>> EvalContext<'a, 'tcx, M> {
         let basic_block = &mir.basic_blocks()[block];
 
         if let Some(stmt) = basic_block.statements.get(stmt_id) {
-            let mut new = Ok(0);
+            let mut new = Ok(false);
             ConstantExtractor {
                 span: stmt.source_info.span,
                 instance: self.frame().instance,
                 ecx: self,
                 mir,
-                new_constants: &mut new,
+                new_constant: &mut new,
             }.visit_statement(
                 block,
                 stmt,
@@ -54,22 +54,22 @@ impl<'a, 'tcx, M: Machine<'tcx>> EvalContext<'a, 'tcx, M> {
                     statement_index: stmt_id,
                 },
             );
-            // if ConstantExtractor added new frames, we don't execute anything here
+            // if ConstantExtractor added a new frame, we don't execute anything here
             // but await the next call to step
-            if new? == 0 {
+            if !new? {
                 self.statement(stmt)?;
             }
             return Ok(true);
         }
 
         let terminator = basic_block.terminator();
-        let mut new = Ok(0);
+        let mut new = Ok(false);
         ConstantExtractor {
             span: terminator.source_info.span,
             instance: self.frame().instance,
             ecx: self,
             mir,
-            new_constants: &mut new,
+            new_constant: &mut new,
         }.visit_terminator(
             block,
             terminator,
@@ -78,9 +78,9 @@ impl<'a, 'tcx, M: Machine<'tcx>> EvalContext<'a, 'tcx, M> {
                 statement_index: stmt_id,
             },
         );
-        // if ConstantExtractor added new frames, we don't execute anything here
+        // if ConstantExtractor added a new frame, we don't execute anything here
         // but await the next call to step
-        if new? == 0 {
+        if !new? {
             self.terminator(terminator)?;
         }
         Ok(true)
@@ -253,43 +253,35 @@ impl<'a, 'tcx, M: Machine<'tcx>> EvalContext<'a, 'tcx, M> {
     }
 }
 
-// WARNING: This code pushes new stack frames.  Make sure that any methods implemented on this
-// type don't ever access ecx.stack[ecx.cur_frame()], as that will change. This includes, e.g.,
-// using the current stack frame's substitution.
-// Basically don't call anything other than `load_mir`, `alloc_ptr`, `push_stack_frame`.
 struct ConstantExtractor<'a, 'b: 'a, 'tcx: 'b, M: Machine<'tcx> + 'a> {
     span: Span,
     ecx: &'a mut EvalContext<'b, 'tcx, M>,
     mir: &'tcx mir::Mir<'tcx>,
     instance: ty::Instance<'tcx>,
-    new_constants: &'a mut EvalResult<'tcx, u64>,
+    // Whether a stackframe for a new constant has been pushed
+    new_constant: &'a mut EvalResult<'tcx, bool>,
 }
 
 impl<'a, 'b, 'tcx, M: Machine<'tcx>> ConstantExtractor<'a, 'b, 'tcx, M> {
     fn try<F: FnOnce(&mut Self) -> EvalResult<'tcx, bool>>(&mut self, f: F) {
-        // previous constant errored
-        let n = match *self.new_constants {
-            Ok(n) => n,
+        match *self.new_constant {
+            // already computed a constant, don't do more than one per iteration
+            Ok(true) => return,
+            // no constants computed yet
+            Ok(false) => *self.new_constant = f(self),
+            // error happened, abort the visitor traversing
             Err(_) => return,
-        };
-        match f(self) {
-            // everything ok + a new stackframe
-            Ok(true) => *self.new_constants = Ok(n + 1),
-            // constant correctly evaluated, but no new stackframe
-            Ok(false) => {}
-            // constant eval errored
-            Err(err) => *self.new_constants = Err(err),
         }
     }
 }
 
 impl<'a, 'b, 'tcx, M: Machine<'tcx>> Visitor<'tcx> for ConstantExtractor<'a, 'b, 'tcx, M> {
     fn visit_constant(&mut self, constant: &mir::Constant<'tcx>, location: mir::Location) {
-        self.super_constant(constant, location);
-        match constant.literal {
-            // already computed by rustc
-            mir::Literal::Value { value: &ty::Const { val: ConstVal::Unevaluated(def_id, substs), .. } } => {
-                self.try(|this| {
+        self.try(|this| {
+            this.super_constant(constant, location);
+            match constant.literal {
+                // already computed by rustc
+                mir::Literal::Value { value: &ty::Const { val: ConstVal::Unevaluated(def_id, substs), .. } } => {
                     debug!("global_item: {:?}, {:?}", def_id, substs);
                     let substs = this.ecx.tcx.trans_apply_param_substs(this.instance.substs, &substs);
                     let instance = Instance::resolve(
@@ -304,19 +296,17 @@ impl<'a, 'b, 'tcx, M: Machine<'tcx>> Visitor<'tcx> for ConstantExtractor<'a, 'b,
                         Mutability::Immutable,
                         this.instance.substs,
                     )
-                });
-            }
-            mir::Literal::Value { .. } => {}
-            mir::Literal::Promoted { index } => {
-                let cid = GlobalId {
-                    instance: self.instance,
-                    promoted: Some(index),
-                };
-                if self.ecx.globals.contains_key(&cid) {
-                    return;
                 }
-                let mir = &self.mir.promoted[index];
-                self.try(|this| {
+                mir::Literal::Value { .. } => Ok(false),
+                mir::Literal::Promoted { index } => {
+                    let cid = GlobalId {
+                        instance: this.instance,
+                        promoted: Some(index),
+                    };
+                    if this.ecx.globals.contains_key(&cid) {
+                        return Ok(false);
+                    }
+                    let mir = &this.mir.promoted[index];
                     let size = this.ecx
                         .type_size_with_substs(mir.return_ty, this.instance.substs)?
                         .expect("unsized global");
@@ -346,9 +336,9 @@ impl<'a, 'b, 'tcx, M: Machine<'tcx>> Visitor<'tcx> for ConstantExtractor<'a, 'b,
                         StackPopCleanup::MarkStatic(Mutability::Immutable),
                     )?;
                     Ok(true)
-                });
+                }
             }
-        }
+        });
     }
 
     fn visit_lvalue(
@@ -357,14 +347,14 @@ impl<'a, 'b, 'tcx, M: Machine<'tcx>> Visitor<'tcx> for ConstantExtractor<'a, 'b,
         context: LvalueContext<'tcx>,
         location: mir::Location,
     ) {
-        self.super_lvalue(lvalue, context, location);
-        if let mir::Lvalue::Static(ref static_) = *lvalue {
-            let def_id = static_.def_id;
-            let span = self.span;
-            if let Some(node_item) = self.ecx.tcx.hir.get_if_local(def_id) {
-                if let hir::map::Node::NodeItem(&hir::Item { ref node, .. }) = node_item {
-                    if let hir::ItemStatic(_, m, _) = *node {
-                        self.try(|this| {
+        self.try(|this| {
+            this.super_lvalue(lvalue, context, location);
+            if let mir::Lvalue::Static(ref static_) = *lvalue {
+                let def_id = static_.def_id;
+                let span = this.span;
+                if let Some(node_item) = this.ecx.tcx.hir.get_if_local(def_id) {
+                    if let hir::map::Node::NodeItem(&hir::Item { ref node, .. }) = node_item {
+                        if let hir::ItemStatic(_, m, _) = *node {
                             let instance = Instance::mono(this.ecx.tcx, def_id);
                             this.ecx.global_item(
                                 instance,
@@ -376,18 +366,15 @@ impl<'a, 'b, 'tcx, M: Machine<'tcx>> Visitor<'tcx> for ConstantExtractor<'a, 'b,
                                 },
                                 this.instance.substs,
                             )
-                        });
-                        return;
+                        } else {
+                            bug!("static def id doesn't point to static");
+                        }
                     } else {
-                        bug!("static def id doesn't point to static");
+                        bug!("static def id doesn't point to item");
                     }
                 } else {
-                    bug!("static def id doesn't point to item");
-                }
-            } else {
-                let def = self.ecx.tcx.describe_def(def_id).expect("static not found");
-                if let hir::def::Def::Static(_, mutable) = def {
-                    self.try(|this| {
+                    let def = this.ecx.tcx.describe_def(def_id).expect("static not found");
+                    if let hir::def::Def::Static(_, mutable) = def {
                         let instance = Instance::mono(this.ecx.tcx, def_id);
                         this.ecx.global_item(
                             instance,
@@ -399,11 +386,13 @@ impl<'a, 'b, 'tcx, M: Machine<'tcx>> Visitor<'tcx> for ConstantExtractor<'a, 'b,
                             },
                             this.instance.substs,
                         )
-                    });
-                } else {
-                    bug!("static found but isn't a static: {:?}", def);
+                    } else {
+                        bug!("static found but isn't a static: {:?}", def);
+                    }
                 }
+            } else {
+                Ok(false)
             }
-        }
+        });
     }
 }
